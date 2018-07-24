7f737f669531e70934fce0d680f1e3f1b7b374ad
marcin-slezak <35666945+marcin-slezak@users.noreply.github.com>
2018-07-24 11:16:16 +0200

    remove reference to removed file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppMainMenu from './AppMainMenu'
